5ea4ba4c04d39318227783ea5dfcaf5a1519acd5
manojit-007 <159749926+manojit-007@users.noreply.github.com>
2025-08-20 11:56:25 +0530

    4th part Upload  code

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -334,7 +334,7 @@ const getUserDetails = catchAsyncError(async (req, res, next) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: false,
-    // sameSite: "none",
+    sameSite: "none",
     sameSite: "strict", //   or "strict" for better security
     maxAge: 7 * 24 * 60 * 60 * 1000, //   7 days
   });
